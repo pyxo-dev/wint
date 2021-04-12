d8efb391c8651ab177543f6b6f71597168227017
Ejez <ejez@protonmail.ch>
2021-04-12 15:27:20 +0300

    Setup vuepress for api docs.

---
+++ vuepress.config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: 'en',
+  title: 'Wint API',
+  description: 'API for Wint, easy i18n for your web app.',
+})
