fa481c378aa1cf6a7b1b82dfa24dd7b372bbdf04
Ejez <ejez@protonmail.ch>
2021-04-15 15:31:02 +0300

    Change vuepress config.

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -3,8 +3,8 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
   lang: 'en',
-  title: 'Wint API',
-  description: 'API for Wint, easy i18n for your web app.',
+  title: 'Wint',
+  description: 'Wint, easy i18n for your web app.',
 
   dest: 'docs/dist',
 })
